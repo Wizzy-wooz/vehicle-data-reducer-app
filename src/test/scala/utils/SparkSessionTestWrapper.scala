adaa5f8c49dc6801a48ea1c00f114eb6857f58a0
Wizzy-wooz <elena.vodzyanova@gmail.com>
2022-04-11 13:04:07 +0300

    Updated test app config

--- src/test/scala/utils/SparkSessionTestWrapper.scala
+++ src/test/scala/utils/SparkSessionTestWrapper.scala
@@ -7,7 +7,7 @@ import org.apache.spark.sql.SparkSession
 trait SparkSessionTestWrapper {
   val conf: SparkConf = new SparkConf()
     .setMaster("local[*]")
-    .setAppName(getClass.getSimpleName)
+    .setAppName("Vehicle Data Reducer App Test")
     .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
 
   lazy val spark: SparkSession = {
